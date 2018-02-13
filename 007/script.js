0356dab6aa1eb9362fb46c934800cda67a00a64a
b-sirius <padfoot_07@163.com>
2018-02-13 19:45:20 +0800

    007

---
+++ 007/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let Slide = {
+    data: [
+        ["dont't you", "think", "that", "if i were", "wrong,", "i'd know it?"],
+        ["find what", "you love", "and let it", "kill you."],
+    ]
+}
